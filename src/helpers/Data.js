0ea9653d6d1d6da90ae0be64973694f344322cc5
Aj Candelaria <ajajcandelaria@gmail.com>
2022-04-04 14:44:17 +0400

    Helpers Applied

--- src/helpers/Data.js
+++ src/helpers/Data.js
@@ -1,4 +1,5 @@
 import aj from "../images/projects/aj.jpg";
+import certDatabase from "../images/projects/certDatabase.PNG";
 
 export function getProjects() {
   const data = [
@@ -12,8 +13,8 @@ export function getProjects() {
       repository: "",
       website: "",
       description: "An e-commerce web application for Taytay Thrift Market with store-owner Inventory system.",
-      about: "",
-      screenshots: [],
+      about: "TayThrifts is an e-commerce web application for Taytay Thrift Market with store-owner Inventory system. I am the Lead Developer in this project and this is made for my final project as a one of the requirements of my subject",
+      screenshots: [certDatabase,aj,aj,aj,certDatabase,aj,certDatabase,aj,certDatabase,aj,certDatabase,aj],
     },
     {
       id: 2,
@@ -25,12 +26,12 @@ export function getProjects() {
       repository: "",
       website: "",
       description: "An e-commerce web application for Taytay Thrift Market with store-owner Inventory system.",
-      about: "",
-      screenshots: [],
+      about: "TayThrifts is an e-commerce web application for Taytay Thrift Market with store-owner Inventory system. I am the Lead Developer in this project and this is made for my final project as a one of the requirements of my subject",
+      screenshots: [aj,aj,aj,aj],
     },
     {
       id: 3,
-      title: "SamQuicksal",
+      title: "SamQuicksal3",
       image: aj,
       languages: ["Laravel", "Android", "TailwindCss", "Kotlin", "MySQL"],
       link: "/projects/samquicksal3",
@@ -38,8 +39,8 @@ export function getProjects() {
       repository: "",
       website: "",
       description: "An e-commerce web application for Taytay Thrift Market with store-owner Inventory system.",
-      about: "",
-      screenshots: [],
+      about: "TayThrifts is an e-commerce web application for Taytay Thrift Market with store-owner Inventory system. I am the Lead Developer in this project and this is made for my final project as a one of the requirements of my subject",
+      screenshots: [aj,aj,aj,aj],
     }
   ];
 
--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,17 +1,23 @@
 import React, { useEffect, useState } from "react";
 import style from "./Project.module.css";
 import { getProjects } from "../../helpers/Data";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Link from "react-scroll/modules/components/Link";
 
 function Project() {
   const [project, setProject] = useState([]);
   const { projectName } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const data = getProjects().filter(
       (project) => project.linkName === projectName
     );
-    console.log(data);
+
+    if(data.length === 0) {
+      navigate(`/projects-${projectName}`, { replace: true });
+    }
+
     setProject(...data);
   }, []);
   return (
@@ -29,15 +35,30 @@ function Project() {
           <div>
             <p className={style.project_title}>{project.title}</p>
             <p className={style.project_desc}>{project.description}</p>
-            <div className={style.links_grid}>
-              Github
-            </div>
+            <div className={style.links_grid}>Github</div>
+            <Link to="bottom" spy={true} smooth={true} offset={0} duration={700} className={style.project_scroll_down}>Scroll Down</Link>
           </div>
         </div>
       </div>
-      <div className={style.project_grid}>
+      <div className={`${style.project_grid} bottom`}>
         <div className={style.sidebar}></div>
-        <div className={style.project_info}>a</div>
+        <div className={style.project_info}>
+          <div className={style.project_title}>
+            <p className="text-xl uppercase border border-sideBarMarketplaceButtonsActive w-fit py-2 px-10">
+              {project.title}
+            </p>
+          </div>
+          <p className={style.project_about}>{project.about}</p>
+          <div className={style.project_images}>
+            {project.screenshots && (
+              <>
+                {project.screenshots.map((screenshot, index) => (
+                  <img src={screenshot} alt={screenshot} key={index} />
+                ))}
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
