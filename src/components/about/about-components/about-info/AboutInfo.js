7e91113a7e2b604ddef11c0fc70ff9070964b534
Aj Candelaria <ajajcandelaria@gmail.com>
2024-03-23 13:26:38 +0400

    About me text and image updated

--- src/components/about/about-components/about-info/AboutInfo.js
+++ src/components/about/about-components/about-info/AboutInfo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./AboutInfo.module.css";
-import aj from "../../../../images/about/aj.jpg";
+import aj from "../../../../images/about/aj2.jpg";
 import { motion } from "framer-motion";
 
 function AboutInfo() {
@@ -12,7 +12,7 @@ function AboutInfo() {
   return (
     <div className={style.info}>
       <motion.p variants={fadeRight} initial='hidden' animate='visible' transition={{ duration: .5 }}>
-      Meet AJ Candelaria, a 23-year-old developer passionate about crafting websites, mobile apps, and dabbling in desktop app development. With a knack for building sleek and functional digital solutions, I thrive on creating user-friendly experiences that leave a lasting impression. Whether it's optimizing websites for seamless navigation, designing intuitive mobile apps, or exploring the world of desktop applications, I'm always eager to tackle new challenges. Let's work together to turn your ideas into reality!
+      Hi, I'm AJ Candelaria, a 23-year-old web and mobile app developer driven by a passion for crafting innovative digital solutions. Specializing in both front-end and back-end technologies, I excel at creating sleek and functional websites, intuitive mobile apps, and even delving into desktop application development. With a strong focus on user-centric design and seamless navigation, I'm dedicated to delivering high-quality, dynamic projects that make a lasting impact. Let's collaborate to bring your ideas to life!
       </motion.p>
       <motion.img variants={fadeRight} initial='hidden' animate='visible' transition={{ duration: .5, delay: .2 }} src={aj} alt="aj" />
     </div>
